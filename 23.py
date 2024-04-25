003a240df1a103d75b08b247617ebb92f901d991
luaanHD <luantoniolo94@gmail.com>
2024-04-25 18:59:30 +0000

    23.py

--- 23.py
+++ 23.py
@@ -1,31 +1,14 @@
-N = int(input('Digite um número inteiro: '))
+numero = int(input("Digite um número inteiro: "))
 
-# Encontrando o número primo mais próximo anterior
-primo_anterior = N - 1
+# Encontrando o próximo número primo anterior
+primo_anterior = numero - 1
 while primo_anterior > 1:
     eh_primo = True
-    for i in range(2, primo_anterior):
+    for i in range(2, int(primo_anterior//2) + 1):
         if primo_anterior % i == 0:
             eh_primo = False
             break
     if eh_primo:
+        print(f"O número primo mais próximo (menor que {numero}) é: {primo_anterior}")
         break
     primo_anterior -= 1
-
-# Encontrando o número primo mais próximo posterior
-primo_posterior = N + 1
-while True:
-    eh_primo = True
-    for i in range(2, primo_posterior):
-        if primo_posterior % i == 0:
-            eh_primo = False
-            break
-    if eh_primo:
-        break
-    primo_posterior += 1
-
-# Verificando qual é o primo mais próximo e imprimindo
-if N - primo_anterior <= primo_posterior - N:
-    print(f"O número primo mais próximo de {N} é: {primo_anterior}")
-else:
-    print(f"O número primo mais próximo de {N} é: {primo_posterior}")
